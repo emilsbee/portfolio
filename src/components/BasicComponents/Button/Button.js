0180b7bb3e6fa40acc48359cfe573ac46a1ec22d
emils <emilsbernhards2001@gmail.com>
2021-04-19 21:00:11 +0200

    Mobile button formatting.

--- src/components/BasicComponents/Button/Button.js
+++ src/components/BasicComponents/Button/Button.js
@@ -7,9 +7,9 @@ import "./button.scss";
 const Button = ({text, onClick, backgroundColor, styles}) => {
 
     return (
-        <button id="custom-button" onClick={onClick} style={{backgroundColor, ...styles}}>
+        <div id="custom-button" onClick={onClick} style={{backgroundColor, ...styles}}>
             {text}
-        </button>
+        </div>
     );
 }
  
