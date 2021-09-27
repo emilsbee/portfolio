9603c6ab1c672c6bfe0d8f428ab2f0f159d09082
Emils Bernhards <bernhardsemils@gmail.com>
2021-09-27 08:31:15 +0200

    Updated bata app section

--- components/ProjectSection/BataApp/BataApp.js
+++ components/ProjectSection/BataApp/BataApp.js
@@ -15,11 +15,7 @@ const BataApp = () => {
             </div>
 
             <div className={styles.projectDetails}>
-                {"Mobile application I built in the first module of university for the Batavierenrace organization." +
-                    " Batavierenrace is a multi-stage student running competition in the Twente region of the Netherlands." +
-                    " The app allows participants to explore routes they have to run, chat with Batavierenrace support staff and" +
-                    " use a fun little bonus feature which is a reminder that sends notifications to remind the runner to drink" +
-                    " some water. Download the apk file to explore the app more. In the login screen use email \"emils@gmail.com\" and password \"123456\"."}
+                {"Mobile application I built in the first module of the university for the Batavierenrace organization. Batavierenrace is a relay race for students in the Twente region of the Netherlands. The app allows participants to explore routes they have to run, chat with Batavierenrace support staff, and use a fun little bonus feature which is a reminder that sends notifications to remind the runner to drink some water. Download the APK file to explore the app more. In the login screen use email \"emils@gmail.com\" and password \"123456\"."}
             </div>
 
             <div className={styles.projectLinkContainer}>
